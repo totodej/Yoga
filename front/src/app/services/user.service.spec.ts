d7966da5b6783c6d37fe1462fb96865c54fa76dd
Thomas Dejardins <tdejardins@gmail.com>
2026-01-24 21:45:57 +0100

    add user tests

--- front/src/app/services/user.service.spec.ts
+++ front/src/app/services/user.service.spec.ts
@@ -1,22 +1,52 @@
-import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
-import { expect } from '@jest/globals';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { UserService } from './user.service';
+import { User } from '../interfaces/user.interface';
+
+import { expect, it, describe, beforeEach } from '@jest/globals';
 
 describe('UserService', () => {
   let service: UserService;
+  let httpMock: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientTestingModule],
+      providers: [UserService],
     });
     service = TestBed.inject(UserService);
+    httpMock = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpMock.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve a user by ID', () => {
+    const mockUser: User = {
+      id: 1,
+      email: 'john@example.com',
+      lastName: 'Doe',
+      firstName: 'John',
+      admin: false,
+      password: 'securePassword123',
+      createdAt: new Date('2023-01-01T00:00:00Z'),
+      updatedAt: new Date('2023-01-02T00:00:00Z'), // optional field included
+    };
+
+    service.getById('1').subscribe((user) => {
+      expect(user).toEqual(mockUser);
+    });
+
+    const req = httpMock.expectOne('api/user/1');
+    expect(req.request.method).toBe('GET');
+    req.flush(mockUser);
   });
 });
